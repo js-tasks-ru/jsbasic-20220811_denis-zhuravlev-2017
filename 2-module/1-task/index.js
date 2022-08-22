10a8ad419360fa5b9b1608ad2a544e6ce2bac8cb
Denis Zhuravlev <denis-zhuravlev-2017@mail.ru>
2022-08-22 09:42:10 +0500

    решение первой задачи в теме объекты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries){
+    let sum = 0;
+    for (let key in salaries){
+        let num = salaries[key];
+        if (isFinite(salaries[key])){
+            sum += num
+        } 
+    } return sum
 }
