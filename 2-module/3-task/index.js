63dea3f1d30fee37fea2016c66db9fde5933fbb8
Denis Zhuravlev <denis-zhuravlev-2017@mail.ru>
2022-08-22 18:28:03 +0500

    неполное решение калькулятора

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,21 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = +prompt(`Введите А`);
+    this.b = +prompt(`Введите B`);
+  },
+  sum() {
+    return this.a + this.b
+  },
+  mul(){
+    return this.a * this.b
+  }
 };
+calculator.read(3, 5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
+
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
